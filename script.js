2df254e9d509f851d212cd98bc20454b24104c9b
Atharavnikam <atharavnikam7@gmail.com>
2024-11-28 10:46:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -131,34 +131,79 @@ if (contactForm) {
 
 
 
-function sendMail() {
 
-	
 
-	// var params = {
-	// 	name: document.getElementById('name').value,
-	// 	email: document.getElementById('email').value,
-	// 	message: document.getElementById('message').value
-	// }
 
-	// const serviceID = "service_evf2wim";
-	// const templateID = "template_v085uvl";
 
-	// emailjs.send(serviceID, templateID, params)
-	// 	.then(
-	// 		res => {
-	// 			document.getElementById('name').value = "";
-	// 			document.getElementById('email').value = "";
-	// 			document.getElementById('message').value = "";
 
-	// 			contactSubmitAfter.classList.add('show');
-	// 			formSection.classList.add('hide');
-	// 			contactSection.classList.add('csa-cs');
-	// 			contactForm.classList.add('csa-cf');
 
-	// 		}
-	// 	)
-	// 	.catch((error) => {
-	// 		console.log(error);
-	// 	})
-}
+
+
+
+
+
+
+
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = "Form submitted successfully";
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
